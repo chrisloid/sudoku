4f8d91071b81ecf532a842a29cb0b12c1ad37856
Christian Loidl <c.lo@gmx.at>
2019-01-19 14:36:03 +0100

    clean code

--- src/app/sudoku/sudoku.ts
+++ src/app/sudoku/sudoku.ts
@@ -1,12 +1,12 @@
 export class Sudoku {
     public valuesAsStringArray: Array<any>;
-    public durationSolvedInMillis : number;
+    public durationSolvedInMillis: number;
     public solved: boolean;
     public layout: Array<any>;
 
     constructor(
-        valuesAsStringArray : Array<any>,
-        durationSolvedInMillis : number,
+        valuesAsStringArray: Array<any>,
+        durationSolvedInMillis: number,
         solved: boolean,
         layout: Array<any>
     ) {
